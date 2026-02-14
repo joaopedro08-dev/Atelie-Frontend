e28d01e87ea8506b4eb4cf24e41c7db6fc1e357a
João Pedro <joaopedrodaladeamello@gmail.com>
2026-02-14 00:16:05 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,27 +3,29 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
+
   const token = request.cookies.get('jwtToken')?.value
   const refreshToken = request.cookies.get('refreshToken')?.value
-  const hasAuth = !!(token || refreshToken)  
+
+  const hasSomeCookie = !!(token || refreshToken)
 
   const response = NextResponse.next()
-  response.headers.set('x-middleware-cache', 'no-cache') 
+  response.headers.set('x-middleware-cache', 'no-cache')
 
-  if ((pathname === '/' || pathname === '/signup') && !hasAuth) {
-    const redirectRes = NextResponse.redirect(new URL('/signin', request.url), 303)
+  if (!hasSomeCookie && pathname.startsWith('/admin')) {
+    const redirectRes = NextResponse.redirect(
+      new URL('/signin', request.url),
+      303
+    )
     redirectRes.headers.set('x-middleware-cache', 'no-cache')
     return redirectRes
   }
 
-  if (!hasAuth && pathname.startsWith('/admin')) {
-    const redirectRes = NextResponse.redirect(new URL('/signin', request.url), 303)
-    redirectRes.headers.set('x-middleware-cache', 'no-cache')
-    return redirectRes
-  }
-
-  if (hasAuth && pathname === '/signin') {
-    const redirectRes = NextResponse.redirect(new URL('/admin', request.url), 303)
+  if (!hasSomeCookie && pathname === '/') {
+    const redirectRes = NextResponse.redirect(
+      new URL('/signin', request.url),
+      303
+    )
     redirectRes.headers.set('x-middleware-cache', 'no-cache')
     return redirectRes
   }
