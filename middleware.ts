f46922b8c8de551b478bce4ea68d888add9a6d80
João Pedro <joaopedrodaladeamello@gmail.com>
2026-02-14 00:40:27 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,23 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const response = NextResponse.next()
-  response.headers.set('x-middleware-cache', 'no-cache')
-  return response
+  const { pathname } = request.nextUrl;
+
+  const blockedRoutes = ["/", "/signup"];
+
+  if (blockedRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  const response = NextResponse.next();
+  response.headers.set("x-middleware-cache", "no-cache");
+
+  return response;
 }
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)",
   ],
-}
+};
