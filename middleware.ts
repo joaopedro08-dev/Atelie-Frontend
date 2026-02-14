77898c620cb14599bc6b678528a14236d51f904b
João Pedro <joaopedrodaladeamello@gmail.com>
2026-02-14 00:20:57 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,34 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-
-  const token = request.cookies.get('jwtToken')?.value
-  const refreshToken = request.cookies.get('refreshToken')?.value
-
-  const hasSomeCookie = !!(token || refreshToken)
-
   const response = NextResponse.next()
   response.headers.set('x-middleware-cache', 'no-cache')
-
-  if (!hasSomeCookie && pathname.startsWith('/admin')) {
-    const redirectRes = NextResponse.redirect(
-      new URL('/signin', request.url),
-      303
-    )
-    redirectRes.headers.set('x-middleware-cache', 'no-cache')
-    return redirectRes
-  }
-
-  if (!hasSomeCookie && pathname === '/') {
-    const redirectRes = NextResponse.redirect(
-      new URL('/signin', request.url),
-      303
-    )
-    redirectRes.headers.set('x-middleware-cache', 'no-cache')
-    return redirectRes
-  }
-
   return response
 }
 
